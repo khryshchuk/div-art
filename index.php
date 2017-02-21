6f5e8188c979f7504d1bf4fd1999d83f7ccf077d
khryshchuk <khryshchuk@mail.ru>
2017-02-21 17:05:05 +0200

    1-th edit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>GIT</title>
+	<link href="style.css" rel="stylesheet" />
+</head>
+<body>
+
+<h1>GIT</h1>
+
+<div class="comment">it is comment it is comment it is comment it is comment it is comment </div>
+
+</body>
+</html>
